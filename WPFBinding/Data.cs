d1d658eeb34764939ed7d8ce8c18f8656dfdb539
Eddy Boughioul <eddyboughioul@hotmail.fr>
2020-07-08 03:05:30 +0200

    Ajoutez des fichiers projet.

---
+++ WPFBinding/Data.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WPFBinding
+{
+    public class Data
+    {
+        public string Name { get; set; }
+
+        public DateTime Date { get; set; }
+    }
+}
---
+++ WPFBinding/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using GalaSoft.MvvmLight.Command;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFBinding
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public ObservableCollection<Data> Datas { get; set; }
+
+        public RelayCommand<Data> DeleteCommand { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Datas = new ObservableCollection<Data>();
+            Datas.Add(new Data() { Date = DateTime.Now.AddDays(-2), Name = "Fichier Toto" });
+            Datas.Add(new Data() { Date = DateTime.Now, Name = "Fichier uno" });
+            Datas.Add(new Data() { Date = DateTime.Now.AddDays(-25), Name = "Folder empty" });
+
+            DeleteCommand = new RelayCommand<Data>(DeleteItem);
+
+            this.DataContext = this;
+        }
+
+        private void DeleteItem(Data item)
+        {
+            Datas.Remove(item);
+        }
+    }
+}
